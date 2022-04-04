3860c738174bf40adb63821371d5dacbda439fc5
fgclue <schefferdaudtfelipe@gmail.com>
2022-04-04 14:33:52 -0300

    C++ Simulator DevBuild 1

---
+++ windows/CPPSim/CPPSim/cppsim.bat
@@ -0,0 +1,12 @@
+@echo off
+::    _____     ______
+::   /     \   |
+::  /   __  \  |   |   |
+::  |  | ++ |  |  -+- -+- Simulator
+::  \  |__  /  |   |   |
+::   -------   |______ v1.0.0
+
+:start
+echo C++ Simulator - Type help for help
+echo Hi, What do you want to do today?
+set /p whatd="> "
