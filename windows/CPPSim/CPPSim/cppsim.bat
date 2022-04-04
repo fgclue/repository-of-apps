ecad54042489ffc5e45dc9908c4afb7aa83c62aa
fgclue <schefferdaudtfelipe@gmail.com>
2022-04-04 14:38:14 -0300

    C++ Simulator DevBuild 1.0.1

--- windows/CPPSim/CPPSim/cppsim.bat
+++ windows/CPPSim/CPPSim/cppsim.bat
@@ -5,8 +5,10 @@
 ::  |  | ++ |  |  -+- -+- Simulator
 ::  \  |__  /  |   |   |
 ::   -------   |______ v1.0.0
-
+ 
 :start
 echo C++ Simulator - Type help for help
 echo Hi, What do you want to do today?
 set /p whatd="> "
+
+if %whatd% == "help" goto ynh
